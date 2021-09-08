1edbdf67c852290f417aa65091a617e251d3adb5
Hyungtae Lim <shapelim@kaist.ac.kr>
2021-09-08 15:21:08 +0900

    Complete to SE3 optimization

--- auxiliary/pass_by_address.cpp
+++ auxiliary/pass_by_address.cpp
@@ -7,9 +7,15 @@
 
 using namespace std;
 
-void foo(Eigen::Vector3d *b) {
+void PassByAddress(const Eigen::Vector3d *b) {
     cout << (*b)(0) << ", " << (*b)(1) << ", " << (*b)(2) << endl;
 }
+
+void PassByData(const double* b) {
+    // Pointer array
+    cout << b[0] << ", " << b[1] << ", " << b[2] << endl;
+}
+
 
 void bar(double *b) {
     cout << b[0] << ", " << b[1] << ", " << b[2] << endl;
@@ -17,7 +23,10 @@ void bar(double *b) {
 
 int main() {
     Eigen::Vector3d a(0.0, 1.1, 1.3);
-    foo(&a);
+    PassByAddress(&a);
+
+    Eigen::Vector3d b(-10.0, 1.0, 0.0);
+    PassByData(b.data());
 
     double *ts_est = (double *) malloc(3 * sizeof(double));
     ts_est[0] = 0.0;
--- se3_estimation_malloc.cc
+++ se3_estimation_malloc.cc
@@ -6,6 +6,7 @@
 #include "glog/logging.h"
 #include <vector>
 #include <random>
+#include <iomanip>
 
 using ceres::AutoDiffCostFunction;
 using ceres::CostFunction;
@@ -69,34 +70,7 @@ Eigen::Matrix3d vec2SO3(double*rot_vec) {
     return R;
 }
 
-
-Eigen::Matrix3d vec2SO3(const Eigen::Vector3d& rot_vec) {
-    double mag = 0;
-
-    mag += pow(rot_vec(0), 2);
-    mag += pow(rot_vec(1), 2);
-    mag += pow(rot_vec(2), 2);
-    mag                 = sqrt(mag);
-
-    Eigen::Vector3d u(rot_vec(0) / mag, rot_vec(1) / mag, rot_vec(2) / mag);
-    Eigen::Matrix3d R   = Eigen::Matrix3d::Identity();
-    Eigen::Matrix3d w_x = vec2skew_symm(u);
-    R += sin(mag) * w_x + (1 - cos(mag)) * w_x * w_x;
-    return R;
-}
-
 Eigen::Matrix4d est2SE3(double*rot_vec, double*ts) {
-    Eigen::Matrix3d R = vec2SO3(rot_vec);
-    Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
-    T.block<3, 3>(0, 0) = R;
-
-    T(0, 3) = ts[0];
-    T(1, 3) = ts[1];
-    T(2, 3) = ts[2];
-    return T;
-}
-
-Eigen::Matrix4d est2SE3(const Eigen::Vector3d& rot_vec, const Eigen::Vector3d& ts) {
     Eigen::Matrix3d R = vec2SO3(rot_vec);
     Eigen::Matrix4d T = Eigen::Matrix4d::Identity();
     T.block<3, 3>(0, 0) = R;
@@ -133,9 +107,9 @@ struct SE3Residual {
 //        residual[0] = sqrt(pow(tgt_(0) - (src_(0) - rot_est[2] * src_(1) + rot_est[1] * src_(2) + ts_est[0]), 2)
 //                           + pow(tgt_(1) - (rot_est[2] * src_(0) + src_(1) - rot_est[0] * src_(2) + ts_est[1]), 2)
 //                           + pow(tgt_(2) - (rot_est[1] * src_(0) + rot_est[0] * src_(1) + src_(2) + ts_est[2]), 2));
-        residual[0] = tgt_(0) - (src_(0) - (*rot_est)[2] * src_(1) + (*rot_est)[1] * src_(2) + (*ts_est)[0]);
-        residual[1] = tgt_(1) - ((*rot_est)[2] * src_(0) + src_(1) - (*rot_est)[0] * src_(2) + (*ts_est)[1]);
-        residual[2] = tgt_(2) - (-(*rot_est)[1] * src_(0) + (*rot_est)[0] * src_(1) + src_(2) + (*ts_est)[2]);
+        residual[0] = tgt_(0) - (src_(0) - rot_est[2] * src_(1) + rot_est[1] * src_(2) + ts_est[0]);
+        residual[1] = tgt_(1) - (rot_est[2] * src_(0) + src_(1) - rot_est[0] * src_(2) + ts_est[1]);
+        residual[2] = tgt_(2) - (-rot_est[1] * src_(0) + rot_est[0] * src_(1) + src_(2) + ts_est[2]);
 
         return true;
     }
@@ -143,6 +117,38 @@ struct SE3Residual {
 private:
     const Eigen::Vector3d src_;
     const Eigen::Vector3d tgt_;
+};
+
+struct SE3ResidualYH {
+    SE3ResidualYH(const Eigen::Vector3d &src, const Eigen::Vector3d &tgt) {
+        src_x = src[0];
+        src_y = src[1];
+        src_z = src[2];
+        tgt_x = tgt[0];
+        tgt_y = tgt[1];
+        tgt_z = tgt[2];
+    }
+
+    template<typename T>
+    bool operator()(const T*const quat_est, const T*const ts_est, T* residual) const {
+
+        Eigen::Map<const Eigen::Quaternion<T>> quat(quat_est);
+        Eigen::Matrix<T, 3, 3> rot = quat.toRotationMatrix();
+
+        residual[0] = tgt_x - rot(0,0) * src_x - rot(0,1) * src_y - rot(0,2) * src_z - ts_est[0];
+        residual[1] = tgt_y - rot(1,0) * src_x - rot(1,1) * src_y - rot(1,2) * src_z - ts_est[1];
+        residual[2] = tgt_z - rot(2,0) * src_x - rot(2,1) * src_y - rot(2,2) * src_z - ts_est[2];
+
+        return true;
+    }
+
+private:
+    double src_x;
+    double src_y;
+    double src_z;
+    double tgt_x;
+    double tgt_y;
+    double tgt_z;
 };
 
 using namespace std;
@@ -218,33 +224,63 @@ int main(int argc, char**argv) {
 
         Problem problem;
 
-        Eigen::Vector3d rot_est(0., 0., 0.);
-        Eigen::Vector3d ts_est(0., 0., 0.);
+//        double*rot_est = (double*) malloc(3 * sizeof(double));
+//        rot_est[0] = 0.0;
+//        rot_est[1] = 0.0;
+//        rot_est[2] = 0.0;
+//
+//        double*ts_est = (double*) malloc(3 * sizeof(double));
+//        ts_est[0] = 0.0;
+//        ts_est[1] = 0.0;
+//        ts_est[2] = 0.0;
 
+        Eigen::Quaterniond quat_gt = Eigen::Quaterniond(T_gt.block<3,3>(0, 0));
+        Eigen::Vector3d pos_gt = T_gt.block<3,1>(0,3);
 
+        Eigen::Vector3d pos(0.0, 0.0, 0.0);
+        Eigen::Quaterniond quat = quat_gt.inverse();
+
+        ceres::EigenQuaternionParameterization *quat_param = new ceres::EigenQuaternionParameterization();
+
+//        problem.AddParameterBlock(pos.data(), 3);
+//        problem.AddParameterBlock(quat.coeffs().data(), 4, quat_param);
 
         for (int i = 0; i < num_pts; ++i) {
 
+//            CostFunction*cost_function =
+//                                new AutoDiffCostFunction<SE3Residual, 3, 3, 3>(new SE3Residual(src.col(i), tgt.col(i)));
+//            problem.AddResidualBlock(cost_function, nullptr, rot_est, ts_est);
+
             CostFunction*cost_function =
-                                new AutoDiffCostFunction<SE3Residual, 3, 3, 3>(new SE3Residual(src.col(i), tgt.col(i)));
-            problem.AddResidualBlock(cost_function, nullptr, &rot_est, &ts_est);
+                                new AutoDiffCostFunction<SE3ResidualYH, 3, 4, 3>(new SE3ResidualYH(src.col(i), tgt.col(i)));
+            problem.AddResidualBlock(cost_function, nullptr, quat.coeffs().data(), pos.data());
         }
 
         // Run the solver!
         Solver::Options options;
         options.minimizer_progress_to_stdout = false;
         Solver::Summary summary;
+
+        Eigen::Quaterniond quat_pre = quat;
+        Eigen::Vector3d pos_pre = pos;
+
         Solve(options, &problem, &summary);
 
-        std::cout << summary.BriefReport() << "\n";
-        Eigen::Matrix4d T_est = est2SE3(rot_est, ts_est);
-        Eigen::Matrix4d T_rel = T_est * T_gt.inverse();
+        std::cout << summary.FullReport() << "\n";
 
+        std::cout << std::fixed << std::setprecision(8);
+        std::cout << quat_gt.coeffs().transpose() << " | " << pos_gt.transpose() << std::endl;
+        std::cout << quat_pre.coeffs().transpose() << " | " << pos_pre.transpose() << std::endl;
+        std::cout << quat.coeffs().transpose() << " | " << pos.transpose() << std::endl;
+
+//        Eigen::Matrix4d T_est = est2SE3(rot_est, ts_est);
+//        Eigen::Matrix4d T_rel = T_est * T_gt.inverse();
+//
 //        free(rot_est);
 //        free(ts_est);
-
-        cout << "\033[1;32m ABS. size:"<< calcTranslationError(T_gt) << "m / " << calcRotationError(T_gt) * 180.0 / M_PI << "deg -> ";
-        cout << "Rel error: " << calcTranslationError(T_rel) << "m / " << calcRotationError(T_rel) * 180.0 / M_PI << "deg\033[0m" << endl;
+//
+//        cout << "\033[1;32m ABS. size:"<< calcTranslationError(T_gt) << "m / " << calcRotationError(T_gt) * 180.0 / M_PI << "deg -> ";
+//        cout << "Rel error: " << calcTranslationError(T_rel) << "m / " << calcRotationError(T_rel) * 180.0 / M_PI << "deg\033[0m" << endl;
     }
 
     return 0;
